ed60bb8827b5c976894c7897748a10844cd3d92a
Isaac Svi <yitzchaksviridyuk@gmail.com>
2022-07-26 00:06:37 +0300

    added feedback

--- script.js
+++ script.js
@@ -1,166 +1,163 @@
-const suits = ["♠", "♥", "♦", "♣"];
-const values = [
-  "A",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "J",
-  "Q",
-  "K",
-];
+// DATA
+const suits = ['♠', '♥', '♦', '♣'];
+const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 const cardValueMap = {
-  2: 2,
-  3: 3,
-  4: 4,
-  5: 5,
-  6: 6,
-  7: 7,
-  8: 8,
-  9: 9,
-  10: 10,
-  J: 11,
-  Q: 12,
-  K: 13,
-  A: 14,
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 5,
+    6: 6,
+    7: 7,
+    8: 8,
+    9: 9,
+    10: 10,
+    J: 11,
+    Q: 12,
+    K: 13,
+    A: 14,
 };
 
+// CLASSES
 class Card {
-  constructor(suit, value) {
-    this.suit = suit;
-    this.value = value;
-  }
+    constructor(suit, value) {
+        this.suit = suit;
+        this.value = value;
+    }
 
-  get color() {
-    return this.suit === "♣" || this.suit === "♠" ? "black" : "red";
-  }
+    get color() {
+        return this.suit === '♣' || this.suit === '♠' ? 'black' : 'red';
+    }
 
-  getHTML() {
-    const cardDiv = document.createElement("div");
-    cardDiv.innerText = this.suit + this.value;
-    cardDiv.style.color = this.color;
-    cardDiv.classList.add("card", this.color);
-    cardDiv.dataset.value = `${this.value} ${this.suit}`;
-    return cardDiv;
-  }
+    getHTML() {
+        const cardDiv = document.createElement('div');
+        cardDiv.innerText = this.suit + this.value;
+        cardDiv.style.color = this.color;
+        cardDiv.classList.add('card', this.color);
+        cardDiv.dataset.value = `${this.value} ${this.suit}`;
+        return cardDiv;
+    }
 }
 
 class Deck {
-  constructor(cards = newDeck()) {
-    this.cards = cards;
-  }
-
-  shuffle() {
-    for (let i = this.cards.length - 1; i > 0; i--) {
-      const newIndex = Math.floor(Math.random() * (i + 1));
-      const oldValue = this.cards[newIndex];
-      this.cards[newIndex] = this.cards[i];
-      this.cards[i] = oldValue;
+    constructor(cards = newDeck()) {
+        this.cards = cards;
     }
-  }
+
+    shuffle() {
+        for (let i = this.cards.length - 1; i > 0; i--) {
+            const newIndex = Math.floor(Math.random() * (i + 1));
+            const oldValue = this.cards[newIndex];
+            this.cards[newIndex] = this.cards[i];
+            this.cards[i] = oldValue;
+        }
+    }
 }
 
+// UTILITY FUNCTIONS
 function newDeck() {
-  return suits.flatMap((suit) => {
-    return values.map((value) => {
-      return new Card(suit, value);
+    return suits.flatMap((suit) => {
+        return values.map((value) => {
+            return new Card(suit, value);
+        });
     });
-  });
 }
 
 function initGame() {
-  playerPointsDiv.innerHTML = 0;
-  cpuPointsDiv.innerHTML = 0;
-  playerSide.innerHTML = "";
-  cpuSide.innerHTML = "";
-  let playerDeck = new Deck();
-  playerDeck.shuffle();
-  let cpuDeck = new Deck();
-  cpuDeck.shuffle();
-  return [playerDeck, cpuDeck];
+    playerPointsDiv.innerHTML = 0;
+    cpuPointsDiv.innerHTML = 0;
+    playerSide.innerHTML = '';
+    cpuSide.innerHTML = '';
+    let playerDeck = new Deck();
+    playerDeck.shuffle();
+    let cpuDeck = new Deck();
+    cpuDeck.shuffle();
+    return [playerDeck, cpuDeck];
 }
 
 function round(playerDeck, cpuDeck) {
-  playerDeck;
-  let playerDraw = flipCards(playerDeck);
-  let cpuDraw = flipCards(cpuDeck);
-  playerSide.appendChild(playerDraw.getHTML());
-  cpuSide.appendChild(cpuDraw.getHTML());
+    playerDeck;
+    let playerDraw = flipCards(playerDeck);
+    let cpuDraw = flipCards(cpuDeck);
+    playerSide.appendChild(playerDraw.getHTML());
+    cpuSide.appendChild(cpuDraw.getHTML());
 
-  if (isRoundWinner(playerDraw, cpuDraw) == 1) {
-    playerPointsDiv.innerHTML++;
-  } else if (isRoundWinner(playerDraw, cpuDraw) == 0) {
-    cpuPointsDiv.innerHTML++;
-  }
+    if (isRoundWinner(playerDraw, cpuDraw) == 1) {
+        playerPointsDiv.innerHTML++;
+    } else if (isRoundWinner(playerDraw, cpuDraw) == 0) {
+        cpuPointsDiv.innerHTML++;
+    }
 }
 
 function flipCards(deck) {
-  return deck.cards[Math.floor(Math.random() * deck.cards.length)];
+    return deck.cards[Math.floor(Math.random() * deck.cards.length)];
 }
 
 function isRoundWinner(playerCard, cpuCard) {
-  if (cardValueMap[playerCard.value] > cardValueMap[cpuCard.value]) return 1;
-  else if (cardValueMap[playerCard.value] < cardValueMap[cpuCard.value])
-    return 0;
-  else return;
+    if (cardValueMap[playerCard.value] > cardValueMap[cpuCard.value]) return 1;
+    else if (cardValueMap[playerCard.value] < cardValueMap[cpuCard.value]) return 0;
+    else return;
 }
 
 function isWinner(playerPoints, cpuPoints) {
-  if (playerPoints == 10) {
-    return 1;
-  } else if (cpuPoints == 10) {
-    return 0;
-  }
-  return;
+    if (playerPoints == 10) {
+        return 1;
+    } else if (cpuPoints == 10) {
+        return 0;
+    }
+    return;
 }
 
 function animateTextSize(element) {
-  element.classList.add("animateSize");
-  setTimeout(() => {
-    element.classList.remove("animateSize");
-  }, 3000);
+    element.classList.add('animateSize');
+    setTimeout(() => {
+        element.classList.remove('animateSize');
+    }, 3000);
 }
 
-const cpuSide = document.querySelector(".cpuDeck");
-const playerSide = document.querySelector(".playerDeck");
-const cpuPointsDiv = document.querySelector(".cpuPoints");
-const playerPointsDiv = document.querySelector(".playerPoints");
-const drawButton = document.querySelector(".drawButton");
+// MAIN
+const cpuSide = document.querySelector('.cpuDeck');
+const playerSide = document.querySelector('.playerDeck');
+const cpuPointsDiv = document.querySelector('.cpuPoints');
+const playerPointsDiv = document.querySelector('.playerPoints');
+const drawButton = document.querySelector('.drawButton');
+
 const decks = initGame();
 const playerDeck = decks[0];
 const cpuDeck = decks[1];
-const winTheme = new Audio("./sounds/victory sound.wav");
+const winTheme = new Audio('./sounds/victory sound.wav');
+
 let gameOver;
-drawButton.addEventListener("click", function () {
-  if (gameOver != undefined) {
-    initGame();
-    gameOver = undefined;
-    drawButton.innerHTML = "DRAW A CARD";
-    return;
-  }
-  playerSide.innerHTML = "";
-  cpuSide.innerHTML = "";
-  round(playerDeck, cpuDeck);
-  gameOver = isWinner(playerPointsDiv.innerHTML, cpuPointsDiv.innerHTML);
-  if (gameOver != undefined) {
-    if (gameOver == 1) {
-      drawButton.innerHTML = "Winner!";
-      winTheme.play();
-      animateTextSize(drawButton);
-      drawButton.disabled = true;
-    } else if (gameOver == 0) {
-      drawButton.innerHTML = "Loser...";
-      animateTextSize(drawButton);
-      drawButton.disabled = true;
+
+drawButton.addEventListener('click', function () {
+    if (gameOver != undefined) {
+        initGame();
+        gameOver = undefined;
+        drawButton.innerHTML = 'DRAW A CARD';
+        return;
     }
-    setTimeout(() => {
-      drawButton.innerHTML = "Start a new Game?";
-      drawButton.disabled = false;
-    }, 3000);
-  }
+
+    playerSide.innerHTML = '';
+    cpuSide.innerHTML = '';
+
+    round(playerDeck, cpuDeck);
+
+    gameOver = isWinner(playerPointsDiv.innerHTML, cpuPointsDiv.innerHTML);
+
+    if (gameOver != undefined) {
+        if (gameOver == 1) {
+            drawButton.innerHTML = 'Winner!';
+            winTheme.play();
+            animateTextSize(drawButton);
+            drawButton.disabled = true;
+        } else if (gameOver == 0) {
+            drawButton.innerHTML = 'Loser...';
+            animateTextSize(drawButton);
+            drawButton.disabled = true;
+        }
+
+        setTimeout(() => {
+            drawButton.innerHTML = 'Start a new Game?';
+            drawButton.disabled = false;
+        }, 3000);
+    }
 });
